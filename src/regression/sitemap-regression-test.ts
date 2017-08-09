27842358de50141534179e8a9cc7432828aa4b22
Fabian Keller <login@fabian-keller.de>
2017-08-09 12:20:32 +0200

    Feature: limit concurrent http requests to 5 with customized http agent

--- src/regression/sitemap-regression-test.ts
+++ src/regression/sitemap-regression-test.ts
@@ -6,6 +6,7 @@ import {SitemapEntry} from '../model/sitemap-entry.model';
 import {UrlReplacer} from './url-replacer';
 import {RegressionViolation} from './regression-violation';
 import * as request from 'request';
+import * as http from 'http';
 import {Request, RequestResponse} from 'request';
 import winston = require('winston');
 
@@ -34,13 +35,16 @@ export class SitemapRegressionTest {
         entries = this.filter.filter(entries)
             .do((all: SitemapEntry[]) => winston.info(`About to check ${all.length} URLs`));
 
+        var agent = new http.Agent({maxSockets: 5}); // 5 concurrent connections per origin
+
         return entries.flatMap(entries => entries)
             .map(entry => new SitemapEntry(this.urlReplacer.replace(entry.url)))
             .flatMap((entry: SitemapEntry): any => {
                 return new Observable(observer => {
                     winston.debug(`About to check ${entry.url}`);
                     const req: Request = request(entry.url, {
-                        timeout: 1500
+                        timeout: 1500,
+                        agent: agent
                     }, (error: any, response: RequestResponse, body: any) => {
                         if (error) {
                             observer.error({'msg': `Could not get ${entry.url}`, err: error});
