539702f755a36172fcafc60ddda8725cd62e0abc
Fabian Keller <login@fabian-keller.de>
2017-10-16 21:26:57 +0200

    WIP: fixing console reporter

--- src/regression/result/regression-result.ts
+++ src/regression/result/regression-result.ts
@@ -1,17 +1,22 @@
 import {HttpResponseStack} from '../http-response-stack';
 import {AssertionResult} from '../assertion/assertion-result';
 import {RegressionResultStatus} from './regression-result-status.enum';
+import * as _ from 'lodash';
+import {HttpErrorStack} from '../http-error-stack';
+import {AbstractHttpStack} from '../abstract-http-stack';
+import {TestCase} from '../test-case';
 
 export class RegressionResult {
 
     private _status: RegressionResultStatus;
-    private responseStack: HttpResponseStack;
+    private _stack: AbstractHttpStack;
+    private assertionResults: AssertionResult[];
 
-    private constructor(responseStack: HttpResponseStack) {
-        this.responseStack = responseStack;
+    private constructor(responseStack: AbstractHttpStack) {
+        this._stack = responseStack;
     }
 
-    static errorneous(stack: HttpResponseStack): RegressionResult {
+    static errorneous(stack: HttpErrorStack): RegressionResult {
         const result: RegressionResult = new RegressionResult(stack);
         result._status = RegressionResultStatus.ERROR;
         return result;
@@ -19,6 +24,7 @@ export class RegressionResult {
 
     static fromAssertionResults(stack: HttpResponseStack, assertionResults: AssertionResult[]): RegressionResult {
         const result: RegressionResult = new RegressionResult(stack);
+        result.assertionResults = assertionResults;
         const allAssertionsPassed: boolean = assertionResults.reduce((passed, cur) => passed && cur.hasPassed, true);
         if (allAssertionsPassed) {
             result._status = RegressionResultStatus.SUCCESS;
@@ -28,6 +34,34 @@ export class RegressionResult {
         return result;
     }
 
+    get testCase(): TestCase {
+        return this._stack.testCase;
+    }
+
+    get violationMessages(): string[] {
+        return _(this.assertionResults || [])
+            .map((ar: AssertionResult) => ar.message)
+            .filter((e: string) => !!e)
+            .value();
+    }
+
+    get errorMessages(): string[] {
+        const error: any = this.errorStack.error;
+        return _(_.isArray(error) ? error: [error])
+            // filter empty messages
+            .filter((msg: any) => !!msg)
+            // turn each element to a string
+            .map((err: any) => _.toString(err))
+            .value();
+    }
+
+    get errorStack(): HttpErrorStack {
+        return this._stack as HttpErrorStack;
+    }
+
+    get responseStack(): HttpResponseStack {
+        return this._stack as HttpResponseStack;
+    }
 
     get status(): RegressionResultStatus {
         return this._status;
--- src/reporter/console-reporter.strategy.ts
+++ src/reporter/console-reporter.strategy.ts
@@ -18,17 +18,31 @@ export class ConsoleReporter implements ReporterStrategy {
         console.log(Array(80).join('='));
         console.log(c.bold(`== ${testCase.testCase} ==`));
 
-        // details
-        if (result.hasErrors || result.hasViolations) {
-            console.log(c.bold(`Details:`));
+        if (result.hasErrors) {
+            console.log('Errors:');
             for (const error of result.errors) {
-                console.log(`  [${error.errorCode}] ${error.affectedUrl.url} with error message: ${error.errorMessage}`);
-            }
-            for (const violation of result.violations) {
-                console.log(`  [${violation.statusCode}] ${violation.affectedUrl.url}`
-                    + ((violation.hasError) ? ` with error message: ${violation.errorMessage}` : ''));
+                console.log(`  Error occured for '${error.testCase.targetUrl}': ${error.errorMessages.join(' # ')}`);
             }
         }
+
+        if (result.hasViolations) {
+            console.log('Violations:');
+            for (const violation of result.violations) {
+                console.log(`  Assertion violated for '${violation.testCase.targetUrl}': ${violation.violationMessages.join(' # ')}`);
+            }
+        }
+
+        // fixme: details
+        // if (result.hasErrors || result.hasViolations) {
+        //     console.log(c.bold(`Details:`));
+        //     for (const error of result.errors) {
+        //         console.log(`  [${error.errorCode}] ${error.affectedUrl.url} with error message: ${error.errorMessage}`);
+        //     }
+        //     for (const violation of result.violations) {
+        //         console.log(`  [${violation.statusCode}] ${violation.affectedUrl.url}`
+        //             + ((violation.hasError) ? ` with error message: ${violation.errorMessage}` : ''));
+        //     }
+        // }
 
 
         // summary statistics
