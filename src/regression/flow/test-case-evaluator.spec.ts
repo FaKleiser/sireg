0bf2d40ce33a04d7a990443df4cad8b8bb2d28e5
Fabian Keller <login@fabian-keller.de>
2017-10-18 17:16:09 +0200

    Test: write test for TestCaseEvaluator

---
+++ src/regression/flow/test-case-evaluator.spec.ts
@@ -0,0 +1,72 @@
+import Mock = jest.Mock;
+import {TestCaseEvaluator} from './test-case-evaluator';
+import {AbstractHttpStack} from '../abstract-http-stack';
+import {TestCase} from '../test-case';
+import {RegressionResult} from '../result/regression-result';
+import {HttpResponseStackBuilder} from '../http-response-stack-builder';
+import {RegressionResultStatus} from '../result/regression-result-status.enum';
+import {RequestResponse} from 'request';
+import {AssertionResult} from '../assertion/assertion-result';
+import {Assertion} from '../assertion/assertion';
+
+describe('TestCaseEvaluator', () => {
+
+    let sut: TestCaseEvaluator;
+    let stack: AbstractHttpStack;
+    let testCase: TestCase;
+    let result: RegressionResult;
+    let stackBuilder: HttpResponseStackBuilder;
+    let RequestResponseMock: Mock<RequestResponse>;
+    let MockAssertion: Mock<Assertion>;
+
+    beforeEach(() => {
+        sut = new TestCaseEvaluator;
+        testCase = TestCase.target('http://localhost/example');
+        stackBuilder = new HttpResponseStackBuilder(testCase);
+        RequestResponseMock = jest.fn<RequestResponse>(() => ({
+            statusCode: jest.fn()
+        }));
+        MockAssertion = jest.fn<Assertion>();
+    });
+
+    test('Evaluator returns ERROR result if HTTP errors are in the stack', () => {
+        stack = stackBuilder.withError({code: 404}).build();
+        result = sut.evaluate(stack);
+        expect(result).toBeInstanceOf(RegressionResult);
+        expect(result.status).toBe(RegressionResultStatus.ERROR);
+    });
+
+    test('Evaluator returns SUCCESS result if no assertions are defined in test case', () => {
+        stack = stackBuilder.withResponse(new RequestResponseMock).build();
+        result = sut.evaluate(stack);
+        expect(result).toBeInstanceOf(RegressionResult);
+        expect(result.status).toBe(RegressionResultStatus.SUCCESS);
+    });
+
+    test('Evaluator returns SUCCESS result if all assertions do not fail', () => {
+        stack = stackBuilder.withResponse(new RequestResponseMock).build();
+        testCase.assert(MockAssertion.mockReturnValueOnce(AssertionResult.hasPassed()));
+        testCase.assert(MockAssertion.mockReturnValueOnce(AssertionResult.hasPassed()));
+        testCase.assert(MockAssertion.mockReturnValueOnce(AssertionResult.hasPassed()));
+        testCase.assert(MockAssertion.mockReturnValueOnce(AssertionResult.hasPassed()));
+
+        result = sut.evaluate(stack);
+        expect(MockAssertion).toHaveBeenCalledTimes(4);
+
+        expect(result).toBeInstanceOf(RegressionResult);
+        expect(result.status).toBe(RegressionResultStatus.SUCCESS);
+    });
+
+    test('Evaluator returns VIOLATION result if at least one assertion fails', () => {
+        stack = stackBuilder.withResponse(new RequestResponseMock).build();
+        testCase.assert(MockAssertion.mockReturnValueOnce(AssertionResult.hasPassed()));
+        testCase.assert(MockAssertion.mockReturnValueOnce(AssertionResult.error('Assertion failed')));
+        testCase.assert(MockAssertion.mockReturnValueOnce(AssertionResult.hasPassed()));
+
+        result = sut.evaluate(stack);
+        expect(MockAssertion).toHaveBeenCalledTimes(3);
+
+        expect(result).toBeInstanceOf(RegressionResult);
+        expect(result.status).toBe(RegressionResultStatus.VIOLATION);
+    });
+});
--- src/regression/flow/test-case-evaluator.ts
+++ src/regression/flow/test-case-evaluator.ts
@@ -2,20 +2,22 @@ import {HttpResponseStack} from '../http-response-stack';
 import {RegressionResult} from '../result/regression-result';
 import {AssertionResult} from '../assertion/assertion-result';
 import {HttpErrorStack} from '../http-error-stack';
+import {AbstractHttpStack} from '../abstract-http-stack';
 
 export class TestCaseEvaluator {
 
     /**
      * Executes the assertions of the test case against the actual received HTTP stack and produces a {@link RegressionResult}.
      */
-    public evaluate(stack: HttpResponseStack): RegressionResult {
+    public evaluate(stack: AbstractHttpStack): RegressionResult {
         // first check if we had HTTP errors on the way
         if (stack instanceof HttpErrorStack) {
             return RegressionResult.errorneous(stack);
         }
 
         // now assert
-        const assertionResults: AssertionResult[] = stack.testCase.assertions.map((assert) => assert(stack));
-        return RegressionResult.fromAssertionResults(stack, assertionResults);
+        const responseStack: HttpResponseStack = stack as HttpResponseStack;
+        const assertionResults: AssertionResult[] = stack.testCase.assertions.map((assert) => assert(responseStack));
+        return RegressionResult.fromAssertionResults(responseStack, assertionResults);
     }
 }
