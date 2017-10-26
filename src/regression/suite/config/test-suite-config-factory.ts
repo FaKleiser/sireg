00fba353c25f34ee2e492011e6d799cf884a3cb5
Fabian Keller <login@fabian-keller.de>
2017-10-26 16:39:12 +0200

    Feature: improve TestSuiteConfigFactory to accept config from flat camleCase map

--- src/regression/suite/config/test-suite-config-factory.ts
+++ src/regression/suite/config/test-suite-config-factory.ts
@@ -1,11 +1,83 @@
 import {TestSuiteConfig} from './test-suite-config';
-import {defaultsDeep} from 'lodash';
+import {defaultsDeep, indexOf, kebabCase, map, set} from 'lodash';
 import * as fs from 'fs';
 
 export class TestSuiteConfigFactory {
 
     public static fromFile(configFile: string): TestSuiteConfig {
-        return defaultsDeep(JSON.parse(fs.readFileSync(configFile, 'utf8')), {
+        const testSuiteConfig: TestSuiteConfig = JSON.parse(fs.readFileSync(configFile, 'utf8'));
+        return TestSuiteConfigFactory.defaultSettings(testSuiteConfig);
+    }
+
+    public static fromCommanderOptions(options: { [option: string]: any }): TestSuiteConfig {
+        // 3 dimensions: type/strategy/option (e.g. loader/csv/path)
+        const parsedConfig: any = {};
+
+        for (const option in options) {
+            if (!options.hasOwnProperty(option)) continue;
+            if (undefined === options[option]) continue;
+
+            // parse options
+            const value: any = options[option];
+            const parts: string[] = kebabCase(option).split('-');
+            if (parts.length < 2) {
+                throw new Error(`Expected given option to at least define type and strategy, but found: ${option}`);
+            }
+
+            // get type
+            const type: string = parts[0];
+            if (-1 === indexOf(['loader', 'replacer', 'filter', 'reporter'], type.toLowerCase())) {
+                throw new Error(`Unknown type: ${type}`);
+            }
+            // suffix with 's' as the config keys are plural
+            const configType: string = `${type}s`;
+
+            const strategy: string = parts[1];
+
+            // turn actual config to object
+            const addToConfig: any = {};
+            if (parts.length > 2) {
+                set(addToConfig, `${configType}.${strategy}.${parts.slice(2).join('.')}`, value);
+            } else {
+                set(addToConfig, `${configType}.${strategy}`, {});
+            }
+            defaultsDeep(parsedConfig, addToConfig);
+        }
+
+        return TestSuiteConfigFactory.defaultSettings({
+            testSuite: 'sireg test suite',
+            loaders: map(parsedConfig['loaders'] || [], (options: any, loader: string) => {
+                return {
+                    loader: loader,
+                    options: options,
+                };
+            }),
+            replacers: map(parsedConfig['replacers'] || [], (options: any, replacer: string) => {
+                return {
+                    replacer: replacer,
+                    options: options,
+                };
+            }),
+            reporters: map(parsedConfig['reporters'] || [], (options: any, reporter: string) => {
+                return {
+                    reporter: reporter,
+                    options: options,
+                };
+            }),
+            filters: map(parsedConfig['filters'] || [], (options: any, filter: string) => {
+                return {
+                    filter: filter,
+                    options: options,
+                };
+            }),
+        });
+    }
+
+    /**
+     * Applies sound defaults to the given config.
+     */
+    private static defaultSettings(testSuiteConfig: TestSuiteConfig): TestSuiteConfig {
+        return defaultsDeep(testSuiteConfig, {
             settings: {
                 concurrentRequests: 3,
                 requestTimeout: 3000
